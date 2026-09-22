ac03e4be80cbbf3c619c1ace6455a7dc0fdc5ad3
ameliav2026S <ameliav@vt.edu>
2026-09-22 10:23:26 -0400

    Completed the Tracker and WageTracker classes.

--- src/main/java/com/group40/Tracker.java
+++ src/main/java/com/group40/Tracker.java
@@ -1,5 +1,87 @@
 package com.group40;
 
-public class Tracker {
+/**
+ * Abstract head class for the tracker classes.
+ * This class is used to track the costs of the project.
+ * It also contains the inherited methods for the
+ * extending classes.
+ * 
+ * @author Amelia Vasquez Rosario
+ * @version September 22, 2026
+ */
+
+public abstract class Tracker {
     
+    // fields
+
+    protected String title;
+    protected String description;
+    protected double rent;
+    protected double amenities;
+    protected double groceries;
+
+    // constructor
+
+    /**
+     * Constructor for the Tracker class.
+     * 
+     * @param title     The title for the tracker.
+     * @param description The description for the tracker.
+     * @param rent      The rent cost inputted by the user.
+     * @param amenities The amenities cost inputted by the user.
+     * @param groceries The groceries cost inputted by the user.
+     */
+    public Tracker(String title, String description, double rent, double amenities, double groceries)
+    {
+        this.title = title;
+        this.description = description;
+        this.rent = rent;
+        this.amenities = amenities;
+        this.groceries = groceries;
+    }
+
+    // methods
+
+    /**
+     * Calculates the total expenses for the tracker.
+     * @return The total expenses.
+     */
+    public double getExpenses()
+    {
+        return rent + amenities + groceries;
+    }
+
+    /**
+     * Provides a stub for the result each extending tracker class will implement.
+    */
+   public abstract double getResult();
+
+   /**
+    * Provides the summary of the tracker's result.
+    * It's also used in the session history list.
+    * @return a string with the summary.
+    */
+   public String getSummary()
+   {
+       return title + ": $" + String.format("%.2f", getResult());
+   }
+
+   /**
+    * Provides the title of the tracker.
+    * @return the title of the tracker.
+    */
+   public String getTitle()
+   {
+       return title;
+   }
+
+   /**
+    * Provides the description of the tracker.
+    * @return the description of the tracker.
+    */
+   public String getDescription()
+   {
+       return description;
+   }
+
 }
--- src/main/java/com/group40/WageTracker.java
+++ src/main/java/com/group40/WageTracker.java
@@ -1,5 +1,63 @@
 package com.group40;
 
+/**
+ * The wage tracking tracker class.
+ * This class is used to track the wage inputted by the user.
+ * It subsequently compared their wage to the total expenses
+ * inputted by the user and calculated by the tracker.
+ * 
+ * @author Amelia Vasquez Rosario
+ * @version September 22, 2026
+ */
 public class WageTracker extends Tracker {
     
+    // fields
+
+    private double monthlyWage;
+
+    // constructor
+
+    /**
+     * Constructor for the WageTracker class.
+     * 
+     * @param rent        The rent cost inputted by the user.
+     * @param amenities   The amenities cost inputted by the user.
+     * @param groceries   The groceries cost inputted by the user.
+     * @param monthlyWage The monthly wage inputted by the user.
+     */
+    public WageTracker(double rent, double amenities, double groceries, double monthlyWage)
+    {
+        super("Wage Tracker", "Is your wage enough to cover your expenses?", rent, amenities, groceries);
+        this.monthlyWage = monthlyWage;
+    }
+
+    // methods
+
+    /**
+     * Calculates the result of the tracker.
+     * This method calculates the difference between
+     * the total expenses and the wage inputted by
+     * the user. It returns a positive number if
+     * the wage is greater than the expenses and a
+     * negative number if the expenses are greater
+     * than the wage.
+     * 
+     * @return The result of the tracker.
+     */
+    public double getRemaining()
+    {
+        return monthlyWage - getExpenses();
+    }
+
+    /**    (non-Javadoc)
+     * Provides the result, and overrides
+     * the getResult() method in the Tracker class.
+     * @return The result of the tracker.
+     */
+    @Override
+    public double getResult()
+    {
+        return getRemaining();
+    }
+
 }
