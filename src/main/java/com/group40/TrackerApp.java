c2538d48bc8449d652161483827bd899c78682bd
ameliav2026S <ameliav@vt.edu>
2026-09-23 18:48:25 -0400

    fixing the header.

--- src/main/java/com/group40/TrackerApp.java
+++ src/main/java/com/group40/TrackerApp.java
@@ -19,23 +19,6 @@ public class TrackerApp extends JFrame
         setSize(600, 400);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
-        // --- persistent header: icon + title, always visible ---
-        ImageIcon rawIcon = new ImageIcon(getClass().getResource("/icon.png"));
-        Image scaledImage = rawIcon.getImage().getScaledInstance(40, 40, Image.SCALE_SMOOTH);
-        JLabel headerIcon = new JLabel(new ImageIcon(scaledImage));
-
-        JLabel headerTitle = new JLabel("Tracker App");
-        headerTitle.setFont(headerTitle.getFont().deriveFont(Font.BOLD, 18f));
-        headerTitle.setForeground(new Color(0x2B2233));
-
-        JPanel headerPanel = new JPanel(new FlowLayout(FlowLayout.CENTER, 10, 12));
-        headerPanel.setBackground(new Color(0xF7F5F0));
-        headerPanel.add(headerIcon);
-        headerPanel.add(headerTitle);
-
-        add(headerPanel, BorderLayout.NORTH);
-        // ----------------------------------------------------------
-
         cardLayout = new CardLayout();
         container = new JPanel(cardLayout);
 
