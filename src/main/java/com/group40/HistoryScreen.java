ffdadada6899409318086367da2ccee0511d05b3
hasinip2026s <hasinip@vt.edu>
2026-09-23 21:43:24 -0400

    Update HistoryScreen.java

--- src/main/java/com/group40/HistoryScreen.java
+++ src/main/java/com/group40/HistoryScreen.java
@@ -1,5 +1,42 @@
 package com.group40;
 
-public class HistoryScreen {
-    
+import javax.swing.*;
+import java.awt.*;
+
+/**
+ * The history screen class that will
+ * visually display the session history list.
+ * Each entry shows the summary of a submitted tracker.
+ * 
+ * @author Hasini Pottipati
+ * @version September 23, 2026
+ */
+
+public class HistoryScreen extends JPanel {
+
+    private DefaultListModel<String> listModel;
+
+    public HistoryScreen()
+    {
+        setLayout(new BorderLayout());
+
+        add(new JLabel("Session History"), BorderLayout.NORTH);
+
+        listModel = new DefaultListModel<>();
+        JList<String> historyList = new JList<>(listModel);
+        add(new JScrollPane(historyList), BorderLayout.CENTER);
+    }
+
+    /**
+     * Reloads the list with the current session history.
+     */
+    public void refresh()
+    {
+        listModel.clear();
+
+        for (Tracker tracker : SessionHistory.getEntries()) {
+            listModel.addElement(tracker.getSummary());
+        }
+    }
+
 }
