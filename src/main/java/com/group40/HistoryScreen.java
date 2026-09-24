72672c95e1c0faedde84ef836a5f75339a61bede
Zisafk <zachias.noble@gmail.com>
2026-09-23 21:16:56 -0400

    Add initial HistoryScreen.java layout and function

--- src/main/java/com/group40/HistoryScreen.java
+++ src/main/java/com/group40/HistoryScreen.java
@@ -1,5 +1,36 @@
 package com.group40;
 
-public class HistoryScreen {
-    
+import javax.swing.*;
+import java.awt.*;
+
+public class HistoryScreen extends JPanel {
+    public HistoryScreen() {
+        setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
+        setBorder(BorderFactory.createEmptyBorder(24, 40, 32, 40));
+        setBackground(new Color(0xF7F5F0));
+
+        // icon
+        ImageIcon rawIcon = new ImageIcon(getClass().getResource("/icon.png"));
+        Image scaledImage = rawIcon.getImage().getScaledInstance(80, 80, Image.SCALE_SMOOTH);
+        JLabel iconLabel = new JLabel(new ImageIcon(scaledImage));
+        iconLabel.setAlignmentX(Component.CENTER_ALIGNMENT);
+
+        // title
+        JLabel titleLabel = new JLabel("History");
+        titleLabel.setFont(titleLabel.getFont().deriveFont(Font.BOLD, 28f));
+        titleLabel.setForeground(new Color(0x2B2233));
+        titleLabel.setAlignmentX(Component.CENTER_ALIGNMENT);
+
+        // subtitle
+        JLabel subtitleLabel = new JLabel("Your previous tracker results");
+        subtitleLabel.setFont(subtitleLabel.getFont().deriveFont(13f));
+        subtitleLabel.setForeground(new Color(0x7A7365));
+        subtitleLabel.setAlignmentX(Component.CENTER_ALIGNMENT);
+
+        add(iconLabel);
+        add(Box.createVerticalStrut(14));
+        add(titleLabel);
+        add(Box.createVerticalStrut(4));
+        add(subtitleLabel);
+    }
 }
