bd3082cab83e8a137ebbd2fc22d9117586e3cb58
hasinip2026s <hasinip@vt.edu>
2026-09-23 21:43:36 -0400

    Update SessionHistory.java

--- src/main/java/com/group40/SessionHistory.java
+++ src/main/java/com/group40/SessionHistory.java
@@ -1,5 +1,43 @@
 package com.group40;
 
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * The session history class.
+ * This class stores every tracker the user submits
+ * while the app is open. The history is not saved
+ * to disk, so it resets when the app closes.
+ * 
+ * @author Hasini Pottipati
+ * @version September 23, 2026
+ */
 public class SessionHistory {
-    
+
+    // fields
+
+    private static List<Tracker> entries = new ArrayList<>();
+
+    // methods
+
+    /**
+     * Adds a tracker to the session history.
+     * 
+     * @param tracker The tracker submitted by the user.
+     */
+    public static void addEntry(Tracker tracker)
+    {
+        entries.add(tracker);
+    }
+
+    /**
+     * Provides the trackers in the session history.
+     * 
+     * @return The list of trackers.
+     */
+    public static List<Tracker> getEntries()
+    {
+        return entries;
+    }
+
 }
